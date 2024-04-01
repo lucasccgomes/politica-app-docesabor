a772aebda987ab853325d1e262cc91fe7eb8645a
Lucasccgomes <lucasccgomes@protonmail.com>
2024-04-01 20:30:09 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='Titulo'>Política de Privacidade para DoceSabor</h1>
+
+      <p>Última atualização: 01/04/2024</p>
+
+      <p>A presente política de privacidade tem como objetivo informar a você, usuário do DoceSabor, sobre como suas informações são coletadas, utilizadas e compartilhadas quando você utiliza nosso serviço.</p>
+
+      <h2>1. Informações que Coletamos</h2>
+      <p>Ao utilizar o DoceSabor, podemos pedir que você nos forneça certas informações pessoalmente identificáveis que podem ser usadas para contatá-lo ou identificá-lo. As informações pessoalmente identificáveis podem incluir, mas não se limitam a:</p>
+      <ul>
+        <li>Nome Completo</li>
+        <li>Endereço de Email</li>
+        <li>Número de Telefone</li>
+        <li>Endereço para Entrega</li>
+        <li>Dados de Login do Google</li>
+        <li>Informações de Pagamento através de WebView (que são processadas por um terceiro e não armazenadas por nó</li>
+        <li>Nome Completo</li>
+        <li>Notificações Push (para atualizações de pedidos e promoções)</li>
+      </ul>
+
+      <h2>2. Como Utilizamos as Informações</h2>
+      <p>Usamos as informações que coletamos para:</p>
+      <ul>
+        <li>Processar seus pedidos de delivery</li>
+        <li>Entrar em contato com você, quando necessário</li>
+        <li>Melhorar nosso serviço</li>
+        <li>Cumprir obrigações legais</li>
+       
+      </ul>
+      <h2>3. Compartilhamento de Informações com Terceiros</h2>
+      <p>Não vendemos nem alugamos informações de usuários a terceiros. No entanto, podemos compartilhar informações com:</p>
+      <ul>
+        <li>Prestadores de serviços de pagamento para processar pagamentos</li>
+        <li>Entrar em contato com você, quando necessário</li>
+        <li>Serviços de entrega para entregar os pedidos</li>
+        <li>Autoridades legais, quando requerido por lei</li>
+       
+      </ul>
+      <h2>4. Segurança das Informações</h2>
+      <p>Tomamos medidas razoáveis para proteger as informações fornecidas dos usuários contra perda, roubo e uso não autorizado, divulgação ou modificação.</p>
+     
+
+      <h2>5. Seus Direitos</h2>
+      <p>Você tem o direito de acessar, corrigir, excluir ou transferir suas informações pessoais. Se desejar exercer esses direitos, entre em contato conosco.</p>
+
+      <h2>6. Alterações na Política de Privacidade</h2>
+      <p>Podemos atualizar nossa Política de Privacidade periodicamente. Notificaremos você sobre quaisquer mudanças publicando a nova Política de Privacidade nesta página.</p>
+
+      <h2>7. Contato</h2>
+      <p>Se tiver alguma dúvida sobre esta Política de Privacidade, entre em contato conosco.</p>
+    </>
+  )
+}
+
+export default App
